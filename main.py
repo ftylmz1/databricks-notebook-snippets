c4831bb88251c63e26aa67d431d8ca4d75302ede
ftylmz1 <ftylmz1@gmail.com>
2021-10-01 17:17:58 +0200

    update

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def delete_subfolder(path):
 
   for f in file_list:
     dbutils.fs.rm(f.path, True)
-    
+
+
 def print_recursive(path):
   file_list = dbutils.fs.ls(path)
 
@@ -13,3 +14,15 @@ def print_recursive(path):
   else:
     for f in file_list:
       print_recursive(f.path)
+
+      
+def get_full_path_recursively(path, list_files: list):
+  file_list = dbutils.fs.ls(path)
+
+  if file_list[0].path == path:
+    f = file_list[0]
+    list_files.insert(0, f.path)
+  else:
+    for f in file_list:
+      get_full_path_recursively(f.path, list_files)
+      
